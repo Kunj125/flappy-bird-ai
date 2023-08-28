50baa3bba3c0f4a8fadfd3b2f4a31be6ac83ba34
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-08-28 09:44:10 +0530

    added base and pipe class

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import os
 
@@ -80,9 +81,85 @@ class Bird:
         rect = rotate_img.get_rect(center= self.img.get_rect(topleft = (self.x, self.y)).center)
         win.blit(rotate_img, rect.topleft)
 
-def get_mask(self):
-    return pygame.mask.from_surface(self.img)
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
 
+
+class Base:
+    VEL = 5
+    WIDTH = BASE.get_width()
+    IMG = BASE
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+class Pipe():
+    DIFF = 200
+    VEL = 5
+    
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+        self.diff = 100
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE, False, True)
+        self.PIPE_BOTTOM = PIPE
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.DIFF
+
+    def move(self):
+
+
+        self.x -= self.VEL
+
+    def draw(self, win):
+
+        # draw top
+        win.blit(self.PIPE_TOP, (self.x, self.top))
+        # draw bottom
+        win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+
+    def collide(self, bird, win):
+
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask,top_offset)
+
+        if b_point or t_point:
+            return True
+
+        return False
 
 def draw_window(win, bird):
     win.blit(BACKGROUND, (0,0))
