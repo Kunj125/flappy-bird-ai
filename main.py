a6668a03e9d12e52728e016560b4990b177b1b1c
w92120kd <kunj.dhola@student.manchester.ac.uk>
2023-08-28 10:24:56 +0530

    finished graphics

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ PIPE = pygame.transform.scale2x(
 BACKGROUND = pygame.transform.scale2x(
     pygame.image.load(os.path.join("assets", "bg.png")))
 
-
+pygame.font.init()
+FONT = pygame.font.SysFont('Arial', 50)
 class Bird:
     bird_images = BIRDS
     MAX_ROTATION = 20
@@ -161,23 +162,55 @@ class Pipe():
 
         return False
 
-def draw_window(win, bird):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BACKGROUND, (0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+    text = FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WINDOW_WIDTH - 10 - text.get_width(), 10))
+    base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(200, 200)
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(600)]
+
     win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     run = True
+    score = 0
     clock = pygame.time.Clock()
     while run:
         clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(win , bird)
-        bird.move()
+        rem  = []
+        add_pipe = False
+
+        for pipe in pipes:
+            if pipe.collide(bird, win):
+                pass
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+            pipe.move()
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+        
+        for r in rem:
+            pipes.remove(r)
+
+        if bird.y + bird.img.get_height() >= 730:
+            pass
+
+        base.move()
+        draw_window(win , bird, pipes, base, score)
+        # bird.move()
     pygame.quit()
     quit()
 
